92200d88089b3259d6f705f81da0222b3ff13553
BrionGahl <BrionGahl@gmail.com>
2023-04-23 21:23:03 -0400

    Feature to automatically insert guilds on server join/when booting bot.

--- wow_helper/db/__init__.py
+++ wow_helper/db/__init__.py
@@ -1,6 +1,7 @@
 from typing import Union
 import sqlalchemy
 from sqlalchemy.exc import DatabaseError
+from sqlalchemy.exc import DataError
 from sqlalchemy.engine import URL
 from sqlalchemy import orm
 import sys
@@ -64,7 +65,42 @@ def insert_guild(g_id: int, name: str) -> None:
 
     session.close()
     conn.close()
-    logger.info("Session complete.")
+    logger.info("Transaction complete.")
+
+
+def update_guild(g_id: int, name: Union[str, None] = None, wow_name: Union[str, None] = None, wow_server: Union[str, None] = None) -> None:
+    conn = __connect()
+    session = __create_session(conn)
+
+    logger.info("Updating information on current guild.")
+
+    retrieved = session.query(models.Guilds).get(g_id)
+
+    retrieved.name = name if name else retrieved.name
+    retrieved.wow_name = wow_name if wow_name else retrieved.wow_name
+    retrieved.wow_server = wow_server if wow_server else retrieved.wow_server
+
+    session.commit()
+
+    session.close()
+    conn.close()
+    logger.info("Transaction complete.")
+
+
+def delete_guild(g_id: int) -> None:
+    conn = __connect()
+    session = __create_session(conn)
+    try:
+        logger.info(f"Deleting guild under ID {g_id}.")
+        retrieved = session.query(models.Guilds).get(g_id)
+        session.delete(retrieved)
+        session.commit()
+    except DataError as e:
+        logger.error(f"Failed to delete guild under ID {g_id}.")
+
+    session.close()
+    conn.close()
+    logger.info("Transaction Complete.")
 
 
 def insert_user(guild_id: int, name: str, wow_name: str, wow_server: str) -> None:
@@ -77,4 +113,4 @@ def insert_user(guild_id: int, name: str, wow_name: str, wow_server: str) -> Non
 
     session.close()
     conn.close()
-    logger.info("Session complete.")
+    logger.info("Transaction complete.")
--- wow_helper/events.py
+++ wow_helper/events.py
@@ -7,12 +7,26 @@ from wow_helper import db
 def setup(bot: commands.Bot) -> None:
     message_send_event(bot)
     guild_join_event(bot)
+    guild_remove_event(bot)
+    guild_update_event(bot)
 
 
 def guild_join_event(bot: commands.Bot):
     @bot.event
-    async def on_guild_join(ctx: discord.Guild):
-        db.insert_guild(ctx.id, ctx.name)
+    async def on_guild_join(guild: discord.Guild):
+        db.insert_guild(guild.id, guild.name)
+
+
+def guild_remove_event(bot: commands.Bot):
+    @bot.event
+    async def on_guild_remove(guild: discord.Guild):
+        db.delete_guild(guild.id)
+
+
+def guild_update_event(bot: commands.Bot):
+    @bot.event
+    async def on_guild_update(old_guild: discord.Guild, new_guild: discord.Guild):
+        db.update_guild(old_guild.id, name=new_guild.name)
 
 
 def message_send_event(bot: commands.Bot) -> None:
